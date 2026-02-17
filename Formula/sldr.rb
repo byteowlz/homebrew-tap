c486ab9a5c3a0eba3fb7b15f630398b86b0e5053
Tommy Falkowski <86825018+WismutHansen@users.noreply.github.com>
2026-02-17 14:48:22 +0100

    fix: sldr 0.2.0 checksums and description

--- Formula/sldr.rb
+++ Formula/sldr.rb
@@ -2,7 +2,7 @@
 # frozen_string_literal: true
 
 class Sldr < Formula
-  desc "A Byteowlz tool"
+  desc "Modular markdown presentations powered by slidev"
   homepage "https://github.com/byteowlz/sldr"
   version "0.2.0"
   license "MIT"
@@ -10,27 +10,22 @@ class Sldr < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/byteowlz/sldr/releases/download/v0.2.0/sldr-v0.2.0-x86_64-apple-darwin.tar.gz"
-      sha256 ""
+      sha256 "c64372e90554b196c494126559e12f9d0058cfa1d00d85fcb7a5330bf130fd67"
     end
     if Hardware::CPU.arm?
       url "https://github.com/byteowlz/sldr/releases/download/v0.2.0/sldr-v0.2.0-aarch64-apple-darwin.tar.gz"
-      sha256 ""
+      sha256 "1b3620741cc9e81fad1eaea7090d06593db6b5135218482a6d61199592311ab2"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
       url "https://github.com/byteowlz/sldr/releases/download/v0.2.0/sldr-v0.2.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 ""
-    end
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/byteowlz/sldr/releases/download/v0.2.0/sldr-v0.2.0-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 ""
+      sha256 "a1cb4c8cfc172008cff9b58b2ff3dbe68f434999a7110005cd33267cfe23fce3"
     end
   end
 
   def install
-    # Install all binaries found in the archive
     Dir.glob("*").each do |file|
       next if File.directory?(file)
       next unless File.executable?(file)
